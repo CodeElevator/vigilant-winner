c37d866218f3f9ed6f102ba11c99e9efef8e426d
CodeElevator <codeelevator131@gmail.com>
2022-07-17 23:04:45 +0300

    Initialization

---
+++ algo.py
@@ -0,0 +1,10 @@
+'''
+The 'algo.py' file represents a pretty good algorithm...
+'''
+num = 7
+while True:
+    if (num % 2) == 0:
+        num = num / 2
+    else:
+        num = num * 3 + 1
+    print(num)
